3190a1ff2e1104680463c42736437cf6c03a90f0
Hiral <hiral@complitech.net>
2020-07-02 14:38:51 +0530

    minor changes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,23 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "payroll_production"
 
+  config.action_mailer.raise_delivery_errors = false
+
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.host.to_s }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'hiral@complitech.net',
+    password:             'hiral412',
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
